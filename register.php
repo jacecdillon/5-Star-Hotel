6e3223e813760abbcc2a55d3e37dd6bed927c5bf
natas15 <nataspijoraitis@gmail.com>
2026-08-25 13:30:38 +0200

    feat: fixed some tiny bugs

--- register.php
+++ register.php
@@ -60,6 +60,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registreren</title>
     <link rel="stylesheet" href="css/register.css">
     <?php include_once 'includes/nav.php'; ?>
